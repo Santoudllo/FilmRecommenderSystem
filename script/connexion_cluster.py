ee7fdf8bc8ee27ec0b64182b407199387e915a35
santoudllo <mamadoualimou.diallo@ynov.com>
2024-04-23 13:57:22 +0200

    csv recuperer à partir de cassandra

--- script/connexion_cluster.py
+++ script/connexion_cluster.py
@@ -5,27 +5,31 @@ def retrieve_data_from_cassandra():
     cluster = Cluster(['127.0.0.1'])
     session = cluster.connect('movies_keyspace')
 
-    #  requête pour récupérer toutes les données de la table movies
+    # Exemple de requête pour récupérer toutes les données de la table movies
     query = "SELECT * FROM movies"
     result = session.execute(query)
 
     # Convertir le résultat en liste de dictionnaires pour une manipulation facile
-    data = []
-    for row in result:
-        data.append(row)
+    data = [row._asdict() for row in result]
 
     return data
+
+def save_to_csv(data, file_path):
+    if data:
+        fieldnames = data[0].keys()
+        with open(file_path, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+        print("Les données ont été sauvegardées sous forme de fichier CSV avec succès.")
+    else:
+        print("Aucune donnée à sauvegarder.")
 
 # Appel de la fonction pour récupérer les données de Cassandra
 data_from_cassandra = retrieve_data_from_cassandra()
 
-# Afficher le total des lignes récupérées
-total_rows = len(data_from_cassandra)
-print("Total des lignes récupérées :", total_rows)
+# Chemin du fichier CSV de sortie
+output_file_path = "/home/santoudllo/Desktop/PROJET_PERSO/FilmRecommenderSystem/data/movies_data.csv"
 
-# Afficher le nombre de colonnes récupérées dans la première ligne
-if total_rows > 0:
-    num_columns = len(data_from_cassandra[0])
-    print("Nombre de colonnes récupérées :", num_columns)
-else:
-    print("Aucune donnée récupérée.")
+# Sauvegarde des données dans un fichier CSV
+save_to_csv(data_from_cassandra, output_file_path)
